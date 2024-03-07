b26fdc27b057bee5fada54af354ec83d9877b8da
hyonie <dev.hyonie@gmail.com>
2024-03-07 15:16:26 +0900

    feat: 포인터

---
+++ c-study/pointer.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+	int b = 100;
+	int* pb = &b; //*주소값을 저장하는 변수임을 나타냄. 
+
+	// b의 주소 값
+	// printf("b = %d\n", &b);
+
+	// 10진수로 출력
+	// 동일한 주소값이 출력됨.
+	printf("b = %p\n", &b);
+	printf("pb = %p\n", pb);
+	printf("*pb = %d\n", *pb); // pd가 가르키는 실제값이 참조가됨.
+
+	
+	
+}
