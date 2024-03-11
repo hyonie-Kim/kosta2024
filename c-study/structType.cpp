9a20fbd02eaa4052f844c8b61ad921fce8c3f4ae
hyonie <dev.hyonie@gmail.com>
2024-03-11 15:17:50 +0900

    feat: test

---
+++ c-study/structType.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct student
+{
+	// 18bite 생성
+	char name[10];
+	int age;
+	int height;
+
+
+}st1, str2;
+// 2개의 변수를 미리 선언할수 있음
+
+void main()
+{
+	// 구조체 타입으로 변수 선언
+	//student a;
+
+	// 멤버 선택해서 값을 대입
+	// 	a.age = 10;
+
+
+
+}
