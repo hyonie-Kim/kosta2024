6e36f761e919db4195b66f1f18b94fc40be20a8d
hyonie <dev.hyonie@gmail.com>
2024-04-05 17:50:39 +0900

    docs: 오버로딩 복습

--- c-study/cPluseglobal.cpp
+++ c-study/cPluseglobal.cpp
@@ -1,3 +1,4 @@
+//#include <stdio.h>
 #include <iostream>
 
 //int nTemp = 10;
@@ -21,7 +22,7 @@
 //}
 
 
-using namespace std;
+//using namespace std;
 // c기반은 전달인자와 상관없이 함수 이름 기준으로 호출
 // c기반은 오버로딩이 안되기 때문에 같은 기능의 함수를 만들더라도
 // 이름을 달리해서 만들어야한다.
@@ -52,6 +53,7 @@ using namespace std;
 
 //c++ 기반으로 오버로딩
 
+/*
 int Add(int a, int b)
 {
 	return a + b;
@@ -76,3 +78,30 @@ void main()
 
 
 }
+
+*/
+
+
+/*
+* C언어에서는 오버로딩이 안됨.
+int Add(int a, int b) {
+	return a + b;
+}
+
+double Add(double a, double b) {
+	return a + b;
+}
+
+char Add(char a, char b) {
+	return a + b;
+}
+
+void main()
+{
+	
+	printf(" %d\n", Add(10, 20));
+	printf(" %f\n", Add(3.14, 1.59));
+
+}
+
+*/
--- c-study/localVariable.cpp
+++ c-study/localVariable.cpp
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include <iostream>
+
+/*
 void func1();
 void func2();
 void main(void)
@@ -20,4 +23,36 @@ void func2()
 {
 	int local = 20;
 	printf("세 번째 메모리 할당 : local = 20\n");
+}
+
+*/
+
+
+
+/*
+* 지역변수가 전역변수보다 우선권을 갖는다.
+int nTemp = 10;
+
+void main()
+{
+	{
+		int nTemp = 20;
+		printf("nTemp의 값은 %d이다.\n", nTemp);
+	}
+	printf("nTemp의 값은 %d이다.\n", nTemp);
+
+
+}
+*/
+
+// C++ 에서는 전역변수를 지역에서 사용하는 것이 가능.
+int nTemp = 10;
+
+using namespace std;
+void main() {
+	{
+		int nTemp = 20;
+		cout << "nTemp = (지역) " << nTemp << endl;
+		cout << "nTemp = (전역스코프) " << ::nTemp << endl;
+	}
 }
