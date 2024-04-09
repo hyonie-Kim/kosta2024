a79a523f71edadc28d7567c7ff2b3eaa01fd3821
hyonie <dev.hyonie@gmail.com>
2024-04-09 09:35:07 +0900

    feat: 동적 싱글톤 패턴

--- c-study/singletonPattern/Singleton.cpp
+++ c-study/singletonPattern/Singleton.cpp
@@ -5,26 +5,56 @@ using namespace std;
 class SingletonClass {
 private:
 	static SingletonClass* instance;
-	SingletonClass() {}
-	~SingletonClass() {}
+	int total;
 	
 public:
-
-	static SingletonClass* getInstance() {
+	SingletonClass() {
+		total = 0;
+	}
+	~SingletonClass() {
+		cout << "¼Ò¸êÀÚ È£Ãâ"<< endl;
+	}
+	static SingletonClass* getInstance()
+	{
 		if (instance == NULL)
 			instance = new SingletonClass();
 		return instance;
 	}
 	void ShowMessage() {
-		cout << "½Ì±ÛÅæÆÐÅÏ ½ÇÇà" << endl;
+		cout << "µ¿Àû ½Ì±ÛÅæ ½ÇÇà" << endl;
 	}
-
+	void addValue(int value) {
+		total = total + value;
+	}
+	int getTotalValue() {
+		return total;
+	}
+	void destroy()
+	{
+		if (instance != NULL)
+		{
+			delete instance;
+			instance = NULL;
+			cout << "instance ÇØÁ¦" << endl;
+		}
+	}
 };
 
 SingletonClass* SingletonClass::instance = NULL;
 
 void main()
 {
-	SingletonClass::getInstance() -> ShowMessage();
+	SingletonClass* ins1 = SingletonClass::getInstance();
+	SingletonClass* ins2 = SingletonClass::getInstance();
+	SingletonClass* ins3 = SingletonClass::getInstance();
+	
+	ins1->addValue(10);
+	cout << "total : " << ins1->getTotalValue() << endl;
+	ins2->addValue(10);
+	cout << "total : " << ins2->getTotalValue() << endl;
+	ins3->addValue(10);
+	cout << "total : " << ins3->getTotalValue() << endl;
+
+	SingletonClass::getInstance() -> destroy();
 	
 }
