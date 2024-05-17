c6e2eeec7b264f5543fc3c5081b09d09e319c81c
hyonie <dev.hyonie@gmail.com>
2024-05-17 17:41:41 +0900

    feat: 리액트의 동작원리

--- react276/reacttest/src/App.js
+++ react276/reacttest/src/App.js
@@ -1,66 +1,75 @@
 // import './App.css';
-import { Component } from 'react';
+//import { Component } from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">Hello React!</header>
-//     </div>
-//   );
-// }
+function App() {
+  let title = '내일 할일';
+  const style = {
+    // backgroundColor: 'black',
+    color: 'black',
+    fontSize: '40px',
+  };
+
+  return (
+    <div style={style}>
+      <h1>{title}</h1>
+      <input type="text"></input>
+      {/* <header className="App-header">Hello React!</header> */}
+    </div>
+  );
+}
 
 // 상속 받은 클래스형 컴포넌트
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Subject />
-        <Menu />
-        <Content />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Subject />
+//         <Menu />
+//         <Content />
+//       </div>
+//     );
+//   }
+// }
 
-export class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>FrontEnd</h1>
-      </header>
-    );
-  }
-}
+// export class Subject extends Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>FrontEnd</h1>
+//       </header>
+//     );
+//   }
+// }
 
-export class Menu extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="">HTML</a>
-          </li>
-          <li>
-            <a href="">CSS</a>
-          </li>
-          <li>
-            <a href="">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+// export class Menu extends Component {
+//   render() {
+//     return (
+//       <nav>
+//         <ul>
+//           <li>
+//             <a href="">HTML</a>
+//           </li>
+//           <li>
+//             <a href="">CSS</a>
+//           </li>
+//           <li>
+//             <a href="">JavaScript</a>
+//           </li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
 
-export class Content extends Component {
-  render() {
-    return (
-      <description>
-        <h2>HTML이란</h2>
-        HTML is Hypertext Markup Language
-      </description>
-    );
-  }
-}
+// export class Content extends Component {
+//   render() {
+//     return (
+//       <description>
+//         <h2>HTML이란</h2>
+//         HTML is Hypertext Markup Language
+//       </description>
+//     );
+//   }
+// }
 
 export default App;
