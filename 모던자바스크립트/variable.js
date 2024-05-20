c2574c00049a61c131bd11e6c767fa9cdf46e9c8
hyonie <dev.hyonie@gmail.com>
2024-05-20 17:51:01 +0900

    feat: 모던 자바스크립트

---
+++ 모던자바스크립트/variable.js
@@ -0,0 +1,40 @@
+/** 
+//문자열입력
+var msg = 'Hello';
+var count = 100;
+
+// 정수로 변환
+msg = count;
+console.log(msg);
+*/
+
+var nan = '난' / 10;
+console.log(nan);
+
+// 문자열 lang,love 주소값이 들어감
+var lang = 'javascript';
+var love = '사랑하';
+var num = 5;
+
+// 널값은 포인터 변수일때 사용, you는 주소값이 없다는것
+var you = null;
+
+console.log(lang + null + love + num);
+
+// 이 문자열의 길이
+var msg =
+  '프레임워크의 일부를 사용하는 것부터 사용자 지정 컴포넌트 및 레이아웃에 이르기까지 다양한 예시를 통해 프로젝트를 빠르게 시작하세요.';
+console.log(msg.length);
+
+// 따옴표 사용
+var msg1 = 'she said "Good bye"';
+var msg2 = "she's gone";
+console.log(msg1);
+console.log(msg2);
+
+// 불리언
+var bigger = 100 > 50;
+console.log(bigger);
+
+var bigger = 100 < 50;
+console.log(bigger);
