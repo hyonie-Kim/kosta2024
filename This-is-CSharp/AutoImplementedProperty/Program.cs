fee222aaead930d6c375db81484ef19757ae3451
hyonie <dev.hyonie@gmail.com>
2024-06-07 15:13:17 +0900

    feat: 프로퍼티 자동생성

---
+++ This-is-CSharp/AutoImplementedProperty/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplementedProperty
+{
+    internal class Program
+    {
+        class BirthdayInfo
+        {
+            public string Name { get; set; } = "Unknown";
+            public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+            public int Age
+            {
+                get
+                {
+                    return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+                }
+            }
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
+            birth.Name = "효니";
+            birth.Birthday = new DateTime(1990, 11, 20);
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"birth.Birthday.ToShortDateString()");
+            Console.WriteLine($"Age : {birth.Age}");
+
+
+        }
+    }
+}
---
+++ This-is-CSharp/Property/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    internal class Program
+    {
+        class BirthdayInfo
+        {
+            private string name;
+            private DateTime birthday;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public DateTime Birthday
+            {
+                get
+                {
+                    return birthday;
+                }
+                set
+                {
+                    birthday = value;
+                }
+            }
+
+            public int Age
+            {
+                get
+                {
+                    // 현재까지의 tick 시간
+                    return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+                }
+            }
+
+        }
+        
+       
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "효니";
+            birth.Birthday = new DateTime(1990, 11, 20);
+
+            Console.WriteLine($"Name : {birth.Name}");
+            //현재 DateTime 개체의 값을 해당하는 짧은 날짜 문자열 표현으로 변환
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
+
+        }
+    }
+}
